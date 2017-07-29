5fb64fc6345b4c1f57f1fe22e987948db55c96dd
Anastasios Zouzias <zouzias@gmail.com>
2017-07-29 11:35:04 +0200

    code hygiene

--- testDense.cpp
+++ testDense.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
-
 #include "algorithms/REKSolver.hpp"
 
 using namespace Eigen;
 
 int main() {
-	// A is an (m, n ) random matrix
-	unsigned int m= 100, n = 10;
-	MatrixXd A(m,n);
-	A.setRandom();
 
-	// xopt is a random n-vector
-	RowVector xopt(n);
-	xopt.setRandom();
+    srand((unsigned int) time(0));
+    unsigned int m = 100, n = 10;
 
-	// b = A * x
-	RowVector b = A * xopt;
+    MatrixXd A(m, n);
+    RowVector xopt(n);
+    xopt.setRandom();
+    A.setRandom();
+    RowVector b = A * xopt;
 
-	REKSolver solver = REKSolver();
+    REKSolver solver = REKSolver();
 
-	long ITERS = 1000000;
-	RowVector x = solver.solve(A, b, ITERS);
+    long ITERS = 1000000;
+    RowVector x = solver.solve(A, b, ITERS);
 
-	// Error must be smaller than 0.5
-	x -= xopt;
-	std::cout<< "Error is " << x.norm() << std::endl;
-	assert( x.norm() <= 0.5);
-	std::cout<< "Success..." << std::endl;
+    // Error must be smaller than 0.5
+    RowVector residual = (x - xopt) / std::sqrt(n);
+    std::cout<< "Error is " << residual.norm() << std::endl;
+    assert( residual.norm() <= 0.01);
+    std::cout<< "Success..." << std::endl;
 
-	return 0;
+    return 0;
 }
--- testSparse.cpp
+++ testSparse.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
+#include <random>
 
 #include "algorithms/REKSolver.hpp"
 
 using namespace Eigen;
 
-int main() {
-	unsigned int m= 100, n = 10;
+SparseMatrix<double> randomSparseMatrix(long m, long n, double threshold){
+    std::default_random_engine gen;
+    std::uniform_real_distribution<double> dist(0.0,1.0);
 
-	MatrixXd A(m, n);
-	RowVector xopt(n);
+
+    std::vector<Eigen::Triplet<double> > tripletList;
+    for(int i=0;i<m;++i)
+        for(int j=0;j<n;++j) {
+            auto v_ij = dist(gen);                         //generate random number
+            if(v_ij < threshold)
+                tripletList.emplace_back(Eigen::Triplet<double>(i, j, v_ij));
+        }
+
+    SparseMatrix<double> mat(m, n);
+    mat.setFromTriplets(tripletList.begin(), tripletList.end());
+
+    return mat;
+}
+
+int main() {
+    srand((unsigned int) time(0));
+    unsigned int m = 100, n = 10;
+
+	// SparseMatrix<double, RowMajor> A = randomSparseMatrix(m, n);
+    MatrixXd A(m, n);
+    RowVector xopt(n);
 	xopt.setRandom();
-	A.setRandom();
+    A.setRandom();
 	RowVector b = A * xopt;
 
 	REKSolver solver = REKSolver();
@@ -19,9 +41,9 @@ int main() {
 	RowVector x = solver.solve(A, b, ITERS);
 
 	// Error must be smaller than 0.5
-	x -= xopt;
-	std::cout<< "Error is " << x.norm() << std::endl;
-	assert( x.norm() <= 0.5);
+    RowVector residual = (x - xopt) / std::sqrt(n);
+	std::cout<< "Error is " << residual.norm() << std::endl;
+	assert( residual.norm() <= 0.5);
 	std::cout<< "Success..." << std::endl;
 
 	return 0;
