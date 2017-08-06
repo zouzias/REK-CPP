1741a91de71664331a2d41da0404fecb9f2fa7dd
Anastasios Zouzias <zouzias@gmail.com>
2017-08-06 08:29:52 +0200

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,28 @@
 #include "algorithms/RekSolver.hpp"
 
 int main() {
+	
 	unsigned int m= 100, n = 10;
 
 	Matrix<double, Dynamic, Dynamic> A(m, n);
 	RowVector xopt(n);
 	xopt.setRandom();
 	A.setRandom();
-    RowVector b = A * xopt;
 
+	RowVector b = A * xopt;
+	
 	auto solver = RekSolver();
-
 	long ITERS = 100000;
-
-    RowVector x = solver.solve(A, b, ITERS);
+	
+	RowVector x = solver.solve(A, b, ITERS);
 
 	std::cout << "(x , xopt)" << std::endl;
 	for (unsigned int j = 0 ; j < A.cols(); j++){
 		std::cout << x(j) << " , " << xopt(j) << std::endl;
 	}
-
-    RowVector residual = x - xopt;
+	
+	RowVector residual = x - xopt;
 	std::cout << "Least Squares error: " << residual.norm() << std::endl;
-
-    return 0;
+	
+	return 0;
 }
