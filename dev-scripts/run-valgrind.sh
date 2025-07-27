5b5e317ef388a56cd9025da0c40982a5748fea94
Anastasios Zouzias <zouzias@users.noreply.github.com>
2025-07-27 18:51:17 +0300

    (ci) integrate valgrind (#12)
    * integrate valgrind

---
+++ dev-scripts/run-valgrind.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+
+echo "***************************"
+echo "***************************"
+echo "*      Valgrind           *"
+echo "***************************"
+echo "***************************"
+pushd build
+valgrind --leak-check=full --track-fds=yes --track-origins=yes --leak-check=full --show-leak-kinds=all --error-exitcode=1 bin/test_sparse
+popd
